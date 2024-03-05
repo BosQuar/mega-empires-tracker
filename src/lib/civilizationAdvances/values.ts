115c3a6da098f0750be536d030a7c91b75bc2a8c
Oskar Berg <oskar.o.a.berg@gmail.com>
2024-03-05 08:12:23 +0100

    remove comment

--- src/lib/civilizationAdvances/values.ts
+++ src/lib/civilizationAdvances/values.ts
@@ -50,9 +50,3 @@ export const civilizationAdvances: CivilizationAdvance[] = [
 	...civilizationAdvancesVP3,
 	...civilizationAdvancesVP6
 ];
-
-/* 
-{name: CivilizationAdvances.Cloth_Making,color: { orange: true}, cost: 50, discountRed:0, discountGreen:5,discountBlue: 0,discountOrange: 10,discountYellow: 0,discountedByAdvance: null, discountedByAdvanceNumber:0, victoryPoints:1},
-{name: CivilizationAdvances.Sculpture,color: { blue: true}, cost: 50, discountRed:5, discountGreen:10,discountBlue: 0,discountOrange: 0,discountYellow: 0,discountedByAdvance: null, discountedByAdvanceNumber:0, victoryPoints:1},
-{name: CivilizationAdvances.Naval_Warfare,color: { orange: true}, cost: 160, discountRed:10,discountGreen: 0,discountBlue: 0,discountOrange: 5,discountYellow: 0,discountedByAdvance:CivilizationAdvances.Cloth_Making, discountedByAdvanceNumber:10, victoryPoints:3},
-*/
