3b6f07b89fc46473c6c6ef978940c16065a3216e
Oskar Berg <oskar.o.a.berg@gmail.com>
2024-08-19 15:22:52 +0200

    continued work

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM --platform=linux/arm64 node:20.12.0-alpine AS builder
 WORKDIR /usr/src/app
 
 ARG TZ=Europe/Stockholm
-ARG PUBLIC_HELLO
 
 COPY . /usr/src/app
+COPY package*.json ./
+
 RUN apk --no-cache add curl tzdata
 RUN cp /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 RUN npm install
--- src/db.server.ts
+++ src/db.server.ts
@@ -1,47 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+export const prisma = new PrismaClient();
 
-declare global {
-	// eslint-disable-next-line no-var
-	var __db__: PrismaClient;
-}
+async function main() {}
 
-// this is needed because in development we don't want to restart
-// the server with every change, but we want to make sure we don't
-// create a new connection to the DB with every change either.
-// in production we'll have a single connection to the DB.
-if (process.env.NODE_ENV === 'production') {
-	// prisma = getClient();
-} else {
-	if (!global.__db__) {
-		global.__db__ = getClient();
-	}
-	prisma = global.__db__;
-}
-
-function getClient() {
-	const DATABASE_URL =
-		process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5434/postgres';
-
-	const databaseUrl = new URL(DATABASE_URL);
-
-	console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
-	// NOTE: during development if you change anything in this function, remember
-	// that this only runs once per server restart and won't automatically be
-	// re-run per request like everything else is. So if you need to change
-	// something in this file, you'll need to manually restart the server.
-	const client = new PrismaClient({
-		datasources: {
-			db: {
-				url: databaseUrl.toString()
-			}
-		}
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
 	});
-	// connect eagerly
-	client.$connect();
-
-	return client;
-}
-
-export { prisma };
